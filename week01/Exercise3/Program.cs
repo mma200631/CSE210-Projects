9d3df474479aac8cbfef3799ede8ad7c911ed6d3
mma200631 <assumpta.chidinma06@gmail.com>
2025-03-05 16:17:52 +0100

    Excercise 3 Assignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,40 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); 
+        int guess = -1; 
+        int guessCount=0;
+
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            
+            string input = Console.ReadLine();
+            
+            if (int.TryParse(input, out guess))  
+            {
+                guessCount++;
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCount} attempts");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid number.");
+            }
+        }
+
     }
 }
