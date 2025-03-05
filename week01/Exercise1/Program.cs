ca8bd5407b8119cd3362c6aefdbc8d39e9d6f436
mma200631 <assumpta.chidinma06@gmail.com>
2025-03-05 13:08:45 +0100

    Excercise 1 assignment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write ("What is your First Name?");
+        string fname = Console.ReadLine();
+        Console.Write ("What is your Last Name?");
+        string lname = Console.ReadLine();
+        Console.WriteLine ($"Your Name is {lname} , {fname} {lname}");
     }
 }
