5e6b7fb57275a18c50958b88504e0464b860fb82
mma200631 <assumpta.chidinma06@gmail.com>
2025-03-05 14:40:18 +0100

    Excercise 2 Assignment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write ("What is your percentage:");
+        string percentage =Console.ReadLine();
+        int number= int.Parse(percentage);
+        string letter;
+        if (number>=90)
+        {
+            letter="A";
+        }
+
+        else if (number>=80)
+        {
+            letter= "B";
+        }
+        else if (number>=70)
+        {
+           letter ="C";
+        }
+        else if (number>=60)
+        {
+            letter="D";
+        }
+
+        else 
+        {
+            letter="F";
+        }
+        Console.WriteLine($"Grade: {letter}");
+
+        if (number>=70)
+        {
+            Console.WriteLine("CONGRATULATIONS, YOU PASSESD");
+        }
+        else 
+        {
+            Console.WriteLine("PUT IN MORE EFFORT NEXT TIME");
+        }   
     }
 }
