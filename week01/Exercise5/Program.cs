aaedfa169265f78e9d9ec9800c1f050c2e6f024b
mma200631 <assumpta.chidinma06@gmail.com>
2025-03-08 09:17:43 +0100

    Excercise 5 Assignment

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,53 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        string username=UserName();
+        int favnum= UserFavoriteNumber();
+        int squarenumber=SquaredNumber(favnum);
+        DisplayResult(username, squarenumber);
+
+        
     }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+       
+
+        static string UserName()
+        {
+        Console.Write("What is your Name?");
+        string UserName= Console.ReadLine();
+        Console.WriteLine($"Hello {UserName}");
+        return UserName;
+        }
+
+        static int UserFavoriteNumber()
+        {
+            Console.Write("Please Enter your favourite number:");
+            string input= Console.ReadLine();
+            
+            if (int.TryParse(input, out int number))
+        {
+            return number;
+        }
+
+        else{
+            Console.WriteLine("Invalid input");
+            return UserFavoriteNumber();
+        }
+        }
+
+        static int SquaredNumber(int favouriteNumber)
+        {
+            return favouriteNumber* favouriteNumber;
+        }
+
+        static void DisplayResult(string name, int SquaredNumber)
+        {
+            Console.WriteLine($"{name} the square of your number is {SquaredNumber}");
+    
+        }
 }
